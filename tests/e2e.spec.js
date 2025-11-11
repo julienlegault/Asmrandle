9f3ce66a65cbfc47a6bcf0ca1c6056be9dec1b6b
julienlegault <julien.p.legault@gmail.com>
2025-11-10 23:51:07 -0500

    Ok actually fixed the e2e tests

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -14,8 +14,8 @@ test.describe('Asmrandle E2E Tests', () => {
         
         // Check footer elements
         await expect(page.locator('.footer')).toBeVisible();
-        await expect(page.locator('footer a img[alt*="LinkedIn"]')).toBeVisible(); // LinkedIn icon
-        await expect(page.locator('footer a img[alt*="GitHub"]')).toBeVisible(); // Github icon
+        await expect(page.locator('#linkedin-link')).toBeVisible(); // LinkedIn icon
+        await expect(page.locator('#github-link')).toBeVisible(); // Github icon
     });
 
     test('Daily game button functionality', async ({ page }) => {
